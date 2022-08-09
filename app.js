cf827f56bb6ae585d62a25466e077c779af75c73
Lawliet66 <sarah.baadarani@lau.edu>
2022-08-09 12:51:55 +0300

    Add flash messages to res.locals

--- app.js
+++ app.js
@@ -50,6 +50,8 @@ passport.use(new LocalStrategy(User.authenticate()))
 
 app.use((req,res,next)=>{
     res.locals.user = req.user
+    res.locals.errorMessage = req.flash("error")
+    res.locals.successMessage = req.flash("success")
     next()
 })
 
